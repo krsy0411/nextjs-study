f10cc8f82753f63c4bb4258e33af716d6361bfa4
PANdas <krsy0411@naver.com>
2024-09-22 15:27:50 +0900

    [4-3] Movie Trailers 끝

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,20 @@
+import { Suspense } from "react";
+import MovieInfo from "../../../../components/movie-info/movie-info";
+import MovieVideos from "../../../../components/movie-videos/movie-videos";
+
 export default function MovieDetail({
 	params: { id },
 }: {
 	params: { id: string };
 }) {
-	return <h1>Movie {id}</h1>;
+	return (
+		<div>
+			<Suspense fallback={<h1>Loading movie info</h1>}>
+				<MovieInfo id={id} />
+			</Suspense>
+			<Suspense fallback={<h1>Loading movie videos</h1>}>
+				<MovieVideos id={id} />
+			</Suspense>
+		</div>
+	);
 }
